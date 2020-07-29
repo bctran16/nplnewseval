30e3b5988804addcbeea8e9478ee4e6b0bcb7f97
Bang <bctran@bu.edu>
2020-07-29 13:54:09 -0400

    api authen not functional

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,45 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv')
 
+var AYLIENTextAPI = require('aylien_textapi');
+
+
+dotenv.config();
 const app = express()
+
+var textapi = new AYLIENTextAPI({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
+console.log(`Your API key is ${process.env.API_KEY}`)
+
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+textapi.sentiment({
+    'text': 'John is a very good football player!'
+  }, function(error, response) {
+    if (error === null) {
+      console.log(response);
+    }
+    else {
+        console.log(error);
+    }
+  });
--- webpack.dev.js
+++ webpack.dev.js
@@ -14,6 +14,10 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            }, 
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
             }
         ]
     },
@@ -31,5 +35,9 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         })
-    ]
+    ],
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    }
 }
